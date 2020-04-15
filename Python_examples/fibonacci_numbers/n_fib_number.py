89b31f7012bd5f27473c2a654ebbc429e00f8b05
Andrey Kochetkov <andrey.a.kochetkov@gmail.com>
2020-04-15 16:01:12 +0300

    style(Fib): refactor doc string

--- Python_examples/fibonacci_numbers/n_fib_number.py
+++ Python_examples/fibonacci_numbers/n_fib_number.py
@@ -1,10 +1,9 @@
-from pytest import mark
-
 """
 Дано целое число 1≤n≤40,
 необходимо вычислить n n n-е число Фибоначчи.
 F0=0, F1=1 и Fn = Fn−1 + Fn−2 при n≥2
 """
+from pytest import mark
 
 
 def get_n_fib_number(n: 'int') -> 'int':
